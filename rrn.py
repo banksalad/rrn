59e18bd6ca4f51c8e8560084ab42023815bc6e4c
Sunghyun Hwang <me@sunghyunzz.com>
2018-08-28 18:46:53 +0900

    Say hello to RRN :wave:
    Signed-off-by: Sunghyun Hwang <me@sunghyunzz.com>

---
+++ rrn.py
@@ -0,0 +1,8 @@
+
+
+def is_valid_rrn(rrn: str) -> bool:
+    pass
+
+
+def is_corresponding_rrn(rrn: str, **kwargs) -> bool:
+    pass
---
+++ tests/test_rrn.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestRRN(unittest.TestCase):
+
+    def test_is_valid_rrn(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_is_corresponding_rrn(self):
+        self.assertEqual(1 + 1, 2)
