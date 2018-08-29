60b342033808814ab2fe3676d0578c67aa14dc64
Sunghyun Hwang <me@sunghyunzz.com>
2018-08-29 17:38:16 +0900

    Implement RRN validation
    Signed-off-by: Sunghyun Hwang <me@sunghyunzz.com>

--- rrn.py
+++ rrn.py
@@ -1,7 +1,74 @@
+import itertools
+import re
+from datetime import datetime
+
+
+HYPHEN = re.compile('[-–]')
+
+BIRTH = 0, 6
+BIRTH_DATE_FORMAT = '%y%m%d'
+
+LOC = 7, 9
+MAX_LOC = 97
+
+HASH = 12
+HASH_BASE = 11
+
+
+def _validate_birth(rrn: str) -> bool:
+    try:
+        return datetime.strptime(
+            rrn[slice(*BIRTH)],
+            BIRTH_DATE_FORMAT
+        ) is not None
+    except ValueError:
+        return False
+
+
+def _validate_location(rrn: str) -> bool:
+    try:
+        return int(rrn[slice(*LOC)]) < MAX_LOC
+    except (TypeError, ValueError):
+        return True
+
+
+def _validate_hash(rrn: str) -> bool:
+    try:
+        h = int(rrn[HASH])
+        s = sum(
+            a * int(b) for a, b in zip(
+                itertools.cycle(range(2, 10)),
+                rrn[:HASH]
+            )
+        )
+        expected = (HASH_BASE - (s % HASH_BASE)) % 10
+        return h == expected
+    except IndexError:
+        return True
 
 
 def is_valid_rrn(rrn: str) -> bool:
-    pass
+    """
+    Validate given RRN and returns if it is valid or not.
+    RRN should include at least 6 digits(birthday part).
+    Otherwise, it would be somewhat meaningless.
+
+    :param rrn: RRN string
+    :type rrn: str
+    :return: validity
+    :rtype: bool
+    """
+    try:
+        rrn = HYPHEN.sub('', rrn)
+        assert rrn.isdigit() and len(rrn) >= BIRTH[1]
+
+        return (
+            _validate_birth(rrn) and
+            _validate_location(rrn) and
+            _validate_hash(rrn)
+        )
+    except AssertionError:
+        raise ValueError
 
 
 def is_corresponding_rrn(rrn: str, **kwargs) -> bool:
--- tests/test_rrn.py
+++ tests/test_rrn.py
@@ -1,10 +1,33 @@
 import unittest
 
+import rrn
+
 
 class TestRRN(unittest.TestCase):
 
     def test_is_valid_rrn(self):
-        self.assertEqual(1 + 1, 2)
+        with self.assertRaises(ValueError):
+            rrn.is_valid_rrn('940812 is my birthday')
+
+        s = '9-408-12'
+        for i in range(len(s)):
+            with self.assertRaises(ValueError):
+                rrn.is_valid_rrn(s[:i])
+
+        for expected, s in [
+            (True, '940812'),
+            (False, '000001-2'),
+            (True, '9408122'),
+            (True, '940812200'),
+            (False, '940812299'),
+            (True, '9408121001745'),
+            (False, '9408121001749'),
+            (True, '9408121001751'),
+            (False, '9408121001750'),
+            (True, '9408221001740'),
+            (False, '9408221001741')
+        ]:
+            self.assertEqual(expected, rrn.is_valid_rrn(s))
 
     def test_is_corresponding_rrn(self):
         self.assertEqual(1 + 1, 2)
