27e46fcd4aac7bfb649ff0f498083c6c905ff9e7
Sunghyun Hwang <me@sunghyunzz.com>
2018-08-31 16:24:42 +0900

    Configure module classifiers
    Signed-off-by: Sunghyun Hwang <me@sunghyunzz.com>

--- setup.py
+++ setup.py
@@ -12,5 +12,13 @@ setuptools.setup(
     maintainer='Rainist',
     maintainer_email='engineering' '@' 'rainist.com',
     python_requires='>=3.5',
-    py_modules=['rrn']
+    py_modules=['rrn'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
 )
